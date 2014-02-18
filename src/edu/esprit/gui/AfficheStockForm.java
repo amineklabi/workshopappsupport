516282c1ed87c977ec36b29087c72862004b17cb
Amine <Amine@192.168.92.133>
2014-02-18 12:12:08 +0100

    Updating gui package name

--- src/edu/esprit/gui/AfficheStockForm.java
+++ src/edu/esprit/gui/AfficheStockForm.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package edu.esprit.gui2;
+package edu.esprit.gui;
 
 /**
  *
--- src/edu/esprit/gui/StockModel.java
+++ src/edu/esprit/gui/StockModel.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package edu.esprit.gui2;
+package edu.esprit.gui;
 
 import edu.esprit.dao.StockDAO;
 import edu.esprit.entities.Stock;
